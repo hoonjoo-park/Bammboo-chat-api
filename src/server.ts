ee52c1847ba86ff2203f3577fc3819bb3e7d0c25
hoonjoo94 <hoonjoo94@naver.com>
2023-10-02 01:15:14 +0900

    소켓 서버 기본 핸들러 구현

--- src/server.ts
+++ src/server.ts
@@ -1,29 +1,39 @@
+import cors from "cors";
 import express, { Request, Response } from "express";
 import { Server } from "http";
 import { Socket, Server as SocketIOServer } from "socket.io";
 
-const PORT: number = Number(process.env.PORT) || 3010;
+const PORT: number = Number(process.env.PORT) || 3090;
 const app = express();
 const server: Server = new Server(app);
-const io: SocketIOServer = new SocketIOServer(server);
+const io: SocketIOServer = new SocketIOServer(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+  allowEIO3: true,
+});
+
+app.use(cors());
 
 app.get("/", (req: Request, res: Response) => {
   res.send(`🏄‍♂️  Chat server is running on port ${PORT}`);
 });
 
 io.on("connection", (socket: Socket) => {
-  socket.on("onMessage", (msg: any) => {
-    console.log(msg);
-    // io.emit("onMessage", msg);
-  });
+  console.log("🏓 Socket connected!");
 
-  socket.on("connect", () => {
-    console.log("user connected");
+  socket.on("message", (msg: any) => {
+    console.log(msg);
   });
 
   socket.on("disconnect", () => {
-    console.log("user disconnected");
+    console.log("Socket disconnected...");
   });
+});
+
+io.on("error", (error: any) => {
+  console.error("Socket error:", error);
 });
 
 server.listen(PORT, () => {
